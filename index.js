c22fb08d2bddfb2c56612c8b9411fdf875beeba7
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-03-20 07:16:31 +0700

    add middleware body-parser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Import Routes
 const authRoute = require('./routes/auth');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 // Router Middleware
 app.use('/api/users', authRoute);
